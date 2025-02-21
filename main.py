545c760c35de02ed32c04320979599f834c01003
“RazinKjz” <alin16740@gmail.com>
2025-02-21 17:16:12 +0100

    feat: 🐧 Added polars, and penguins dataset

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from statistics-steam!")
+import polars as pl
 
+
+def main():
+    q = (
+        pl.scan_csv("data/penguins.csv")
+        # .filter(pl.col("sepal_length") > 5)
+        # .group_by("species")
+        # .agg(pl.all().sum())
+    )
+    df = q.collect()
+    print(df)
 
 if __name__ == "__main__":
     main()
